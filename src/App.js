ca8bcc0f5eb2968b7b330e482fe29554e550fc97
Josiah Tsang <jtsang02@student.ubc.ca>
2021-09-13 04:24:19 -0700

    initial commit
    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./css/style.css";
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import NavBar from "./components/navbar";
+import Header from "./components/Header";
+import RenderTable from "./components/RenderTable";
+import History from "./components/History";
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <NavBar />
+        <Header />
+      </div>
+      <div className="hero">
+        <RenderTable onClick ={() => RenderTable()} />
+        <History />
+      </div>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/createArray.js
@@ -0,0 +1,33 @@
+const createArray = (index_y, index_x) => {
+  
+  var n = 12;
+  var th_x = 1, th_y = 1;
+
+  const mainArray = new Array(n + 1); // create 1d array
+
+  for (var z = 0; z < mainArray.length; z++) // loop to create 2d array from 1d array
+    mainArray[z] = new Array(n + 1);
+
+  for (var x = 0; x < n + 1; x++)   // fill 2d array
+    for (var y = 0; y < n + 1; y++)
+      (index_x === x && index_y === y) ? mainArray[x][y] = x * y : mainArray[x][y] = "";
+
+  mainArray[0][0] = "X"; // fill (0,0) with char X
+
+  for (var i = 1; i < mainArray.length; i++) {    // fill x value headers
+
+    mainArray[0][i] = th_x;
+    th_x++;
+  }
+
+  for (var j = 1; j < mainArray.length; j++) {    // fill y value headers
+
+    mainArray[j][0] = th_y;
+    th_y++;
+  }
+
+return mainArray;
+
+};
+
+export default createArray;
