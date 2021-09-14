eb2b2cec5f565decc6c52aee27ef16d6f9d7c4bb
Josiah Tsang <jtsang02@student.ubc.ca>
2021-09-13 23:27:27 -0700

    Revert "Updates"
    This reverts commit 09bd5346bacb7f33dc606b2145fe5dbf34da0ed9.

---
+++ src/components/navbar.jsx
@@ -0,0 +1,9 @@
+const NavBar = () => { // stateless functional component
+    console.log('navbar - rendered');
+
+    return (
+      <nav className="navbar"></nav>
+    );
+}
+
+export default NavBar;
