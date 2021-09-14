09bd5346bacb7f33dc606b2145fe5dbf34da0ed9
Josiah Tsang <jtsang02@student.ubc.ca>
2021-09-13 21:39:47 -0700

    Updates

--- src/components/navbar.jsx
+++
@@ -1,9 +0,0 @@
-const NavBar = () => { // stateless functional component
-    console.log('navbar - rendered');
-
-    return (
-      <nav className="navbar"></nav>
-    );
-}
-
-export default NavBar;
